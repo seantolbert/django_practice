36a46f24e15b78fe105855d507963ddac7f8d248
Sean Tolbert <stolbert4150@gmail.com>
2022-09-05 11:43:54 -0400

    adds functioning global css in root static file + at app level notes/static/notes/notelist.css

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("title", "date")
+
+admin.site.register(Note)
 
 # Register your models here.
